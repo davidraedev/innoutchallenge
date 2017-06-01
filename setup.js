07a8a4803d5d4526d7c994a654987850646d7a7d
daraeman <daraeman@gmail.com>
2017-06-01 10:44:14 -0700

    added env file to setup

--- setup.js
+++ setup.js
@@ -1,18 +1,27 @@
 var fs = require( "fs" );
 
 var mongo_log = "log/mongo.log";
+var env_file = ".env";
+var env_template = ".env.sample";
 
-fs.access( mongo_log, fs.constants.F_OK, ( error ) => {
-
-	if ( ! error )
-		return;
-
+// create mongo log file
+fs.access( mongo_log, fs.constants.F_OK, function( error ) {
+	if ( error )
+		throw new Error( error );
 	fs.writeFile( mongo_log, "", function( error ) {
-
 		if ( error )
-			return console.log( error );
-
-		console.log( "The file was saved!" );
+			throw new Error( error );
+		console.log( "Mongo Log Created." );
 	});
+});
 
+
+// create blank .env file
+fs.access( env_file, fs.constants.F_OK, function( error ) {
+	if ( error ) {		
+		fs.createReadStream( env_template ).pipe( fs.createWriteStream( env_file ) );
+		console.log( "Unpopulated Environment File Created" );
+	}
+	else
+		console.log( "Environment File Already Exists" );
 });
