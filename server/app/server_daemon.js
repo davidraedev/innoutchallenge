a6b6007dd0922afdad7c3af4fdd812889cae8af8
David Rae <daraeman@gmail.com>
2018-03-13 06:09:27 -0700

    add year expires to static content

--- server/app/server_daemon.js
+++ server/app/server_daemon.js
@@ -28,8 +28,8 @@ app.use( cors( {
 
 app.disable( "x-powered-by" );
 
-app.use( "/img", express.static( process.env.BASE + "/server/public/img" ) );
-app.use( "/font", express.static( process.env.BASE + "/server/public/font" ) );
+app.use( "/img", express.static( process.env.BASE + "/server/public/img", { maxage: "1y" } ) );
+app.use( "/font", express.static( process.env.BASE + "/server/public/font", { maxage: "1y" } ) );
 
 var MongoDBStore = require( "connect-mongodb-session" )( session );
 
