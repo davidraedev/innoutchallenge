bd42e38765e32b37647a7fc9d142792a7237a7b4
David Rae <daraeman@gmail.com>
2018-07-07 14:22:25 -0700

    add geolocation component

---
+++ client/components/Geolocation.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { geolocated, geoPropTypes } from "react-geolocated";
+
+class Geolocation extends React.Component {
+
+	componentDidUpdate( old_props ) {
+		if ( JSON.stringify( old_props ) !== JSON.stringify( this.props ) )
+			this.props.handler();
+	}
+
+	render() {
+		return "";
+	}
+}
+
+Geolocation.propTypes = { ...Geolocation.propTypes, ...geoPropTypes };
+
+export default geolocated({
+	positionOptions: {
+		enableHighAccuracy: false,
+	},
+	userDecisionTimeout: 5000,
+	suppressLocationOnMount: true
+})( Geolocation );
